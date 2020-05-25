9d578b74749c7b5ef664f806bdbca0abd7464dd9
Ihor Nedaiev <ihor.nedaiev@nure.ua>
2020-05-25 22:32:08 +0300

    Main GameMechanics done
    Pep, give us some arts!!!

---
+++ DoubleMindGame/Assets/Scripts/CamShake.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamShake : MonoBehaviour
+{
+    public Animation camShake;
+    public void Shake()
+    {
+        Debug.Log("Shake");
+        camShake.Play("shake");
+    }
+}
