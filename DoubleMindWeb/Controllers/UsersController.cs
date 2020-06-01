8694f02c51c7d010e922e27a6acbf374c533b3b6
Ihor Nedaiev <ihor.nedaiev@nure.ua>
2020-06-01 20:33:41 +0300

    Web-Api done

--- DoubleMindWeb/Controllers/UsersController.cs
+++ DoubleMindWeb/Controllers/UsersController.cs
@@ -23,36 +23,17 @@ namespace DoubleMindWeb.Controllers
 
         ApplicationDbContext db = new ApplicationDbContext();
 
-
-
-        /* [HttpGet]
-         public IEnumerable<ApplicationUser> Get()
-         {
-             var mapper = MapHelper.Mapping<Todo, ApplicationUser>();
-             return mapper.Map<List<ApplicationUser>>(this.todoService.GetTodos()); 
-         }
-
-
-         [HttpGet("{id}")]
-         public ApplicationUser Get(int id)
-         {
-             var mapper = MapHelper.Mapping<Todo, ApplicationUser>();
-             return mapper.Map<ApplicationUser>(this.todoService.GetTodoById(id));
-         }*/
-
+/*
         [HttpGet]
         public IEnumerable<ApplicationUser> GetUsers()
         {
             IEnumerable<ApplicationUser> users = db.Users.ToList();
             return users;
-        }
+        }*/
 
         [HttpPost]
-        //[Route("{id}/{password}")]
         public async Task<bool> Login([FromBody] LoginViewModel model)
         {
-            // Сбои при входе не приводят к блокированию учетной записи
-            // Чтобы ошибки при вводе пароля инициировали блокирование учетной записи, замените на shouldLockout: true
 
             ApplicationUserManager userManager = HttpContext.Current.GetOwinContext()
                                                   .GetUserManager<ApplicationUserManager>();
@@ -63,51 +44,40 @@ namespace DoubleMindWeb.Controllers
             if (user != null)
                 return true;
             return false;
-/*  
-                model.RememberMe = false;
-            var result = await SignInManager.PasswordSignInAsync(model.Email.GetUntilOrEmpty(), model.Password, model.RememberMe, shouldLockout: false);*/
-           /* switch (result)
-            {
-                case SignInStatus.Success:
-                    
-                default:
-                    ModelState.AddModelError("", "Failed attempt.");
-                    return false;
-            }*/
         }
-        
-        public ApplicationUser GetUser(string id)
+
+        [HttpGet]
+        public GameUserModels GetUser([FromBody] ApplicationUser usr)
         {
-            ApplicationUser user = db.Users.Find(id);
-            return user;
+            ApplicationUserManager userManager = HttpContext.Current.GetOwinContext()
+                                                  .GetUserManager<ApplicationUserManager>();
+            ApplicationUser user = userManager.FindByName(usr.Email);
+            GameUserModels guser = new GameUserModels(user.UserLevel, user.UserPoints);           
+            if (user != null)
+                return guser;
+            return null;
         }
-/*
-        [HttpPost]
-        public void CreateUser([FromBody]ApplicationUser user)
-        {
-            db.Users.Add(user);
-            db.SaveChanges();
-        }*/
 
         [HttpPut]
-        public void EditUser(string id, [FromBody]ApplicationUser user)
+        public void EditUser([FromBody]ApplicationUser usr)
         {
-            if (id == user.Id)
-            {
-                db.Entry(user).State = EntityState.Modified;
-
-                db.SaveChanges();
-            }
-        }
-
-      /*  public void DeleteBook(string id)
-        {
-            ApplicationUser user = db.Users.Find(id);
+            ApplicationUserManager userManager = HttpContext.Current.GetOwinContext()
+                                                 .GetUserManager<ApplicationUserManager>();
+            ApplicationUser user = userManager.FindByName(usr.Email);
             if (user != null)
             {
-                db.Users.Remove(user);
-                db.SaveChanges();
+
+
+                //db.Entry(user).State = EntityState.Modified;
+
+                user.UserLevel = usr.UserLevel;
+                user.UserPoints = usr.UserPoints;
+
+                userManager.Update(user) ;
+               // db.Users.Attach(user);
+
+               // db.SaveChanges();
             }
-        }*/
+        }
     }
 }
---
+++ DoubleMindWeb/obj/Release/AspnetCompileMerge/TempBuildDir/Flex.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Flex.aspx.cs" Inherits="DoubleMindWeb.Views.Comment" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body style="height: 585px; width: 1359px">
+    <form id="form1" runat="server">
+        <div>
+            <div id="formDesign">
+            <table>
+            <tr><td><p>Comment</p></td>
+                <td><p>
+                    <asp:TextBox ID="txtComment" runat="server" TextMode="MultiLine"></asp:TextBox>
+                    </p></td>
+            </tr>
+            <tr>
+                <td><p>Rating</p></td>
+                <td></td>
+            </tr>
+                <tr><td></td>
+                   <td><asp:Button ID="btn_Submit" runat="server" Text="Comment" BackColor="#3366FF" BorderStyle="Groove" ForeColor="White" Width="102px" /></td>
+                </tr>
+        </table>
+                     </div>
+            <h4 style="text-decoration:underline;">Comments:</h4>
+            <div>
+            <asp:Repeater ID="Repeater1" runat="server" OnItemCommand="Repeater1_ItemCommand">
+                <ItemTemplate>
+                    <div style="margin-top: 15px; border: solid 2px">
+                        <b>
+                            <asp:Label ID="Label1" runat="server" Text='<%#Eval("Name") %>'>'></asp:Label></b>&nbsp;(<asp:Label ID="Label2" runat="server" Text='<%#Eval("Email") %>'>'></asp:Label>):<br />
+                        <asp:Label ID="Label3" runat="server" Text='<%#Eval("Comment") %>'></asp:Label><br />
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+                </div>
+            <div style="overflow: hidden; margin-top: 214px;">
+                <asp:Repeater ID="rptPaging" runat="server" OnItemCommand="rptPaging_ItemCommand">
+                    <ItemTemplate>
+                        <asp:LinkButton ID="btnPage"
+                            Style="padding: 8px; margin: 2px; background: #007acc; border: solid 1px blue; font: 8px;"
+                            CommandName="Page" CommandArgument="<%# Container.DataItem %>"
+                            runat="server" ForeColor="White" Font-Bold="True" CausesValidation="false"><%# Container.DataItem %>
+                        </asp:LinkButton>
+                    </ItemTemplate>
+                </asp:Repeater>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
